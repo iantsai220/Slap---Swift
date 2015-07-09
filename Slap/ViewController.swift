6c229c5222a3c5735ad68f2f87e266f5049cd6fd
iantsai <iantsai86@gmail.com>
2015-07-08 19:08:46 -0700

    working on UIImagePickerController

--- Slap/ViewController.swift
+++ Slap/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imageBarButtonWasPressed(sender: UIBarButtonItem) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            
+            let actionSheet = UIAlertController(title: "123", message: "456", preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            
+            
+        }
+        
+        imagePicker.allowsEditing = true
+        
+        
+        
+    }
+    
 
 }
 
